09f81f74ad49e586ec2ed354481d6c7eba1c730e
RomanHard <86237018+RomanHard@users.noreply.github.com>
2022-09-05 15:58:48 +0000

    Deploying to gh-pages from @ RomanHard/project-team@d37de9e89183cbcf2a838ea6208b31114d4e4018 🚀

---
+++ index.d7dc22ba.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalmobBtn:document.querySelector("[data-modalmob-open]"),closeModalmobBtn:document.querySelector("[data-modalmob-close]"),modalmob:document.querySelector("[data-modalmob]")};function e(){o.modalmob.classList.toggle("is-hidden")}o.openModalmobBtn.addEventListener("click",e),o.closeModalmobBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.d7dc22ba.js.map
